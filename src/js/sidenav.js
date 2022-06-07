5071423abbc9a8bff2a835062f280feec528d1f5
vitorss <vitorss@gmail.com>
2022-06-06 23:19:55 -0300

    add wrapper section

---
+++ src/js/sidenav.js
@@ -0,0 +1,7 @@
+const openNav = () => {
+  document.getElementById("sideNavigation").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("sideNavigation").style.width = "0";
+};
